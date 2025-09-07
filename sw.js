52fcb0840e44caca79fca66b969424ab5c83353b
jackleesus-crypto <jackleesus@gmail.com>
2025-09-07 01:32:41 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Simple offline cache
+const CACHE = 'shadowhud-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
