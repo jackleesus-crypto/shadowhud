dc7a090c02891e10fc0cce32af32bedad695dca2
jackleesus-crypto <jackleesus@gmail.com>
2025-09-11 16:59:09 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,5 @@
-// Offline cache (bump version when shipping new builds)
-const CACHE = 'shadowhud-full-v3';
-const ASSETS = [
-  './','./index.html','./style.css','./app.js','./manifest.json',
-  './icons/icon-192.png','./icons/icon-512.png'
-];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+const CACHE='shadowhud-daily-v1';
+const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
