780c296443ed91e8d0dbc766d838d7bc97ced8fd
jackleesus-crypto <jackleesus@gmail.com>
2025-09-11 18:38:52 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='shadowhud-full-v2';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting());});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const CACHE='shadowhud-full-v3';self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request))});
