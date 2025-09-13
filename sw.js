84feaf867b6d78214448c4234f6e2856ad7e6926
jackleesus-crypto <jackleesus@gmail.com>
2025-09-13 18:24:17 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-self.addEventListener('install', e=>{ self.skipWaiting(); });
-self.addEventListener('activate', e=>{ self.clients.claim(); });
-// very basic offline passthrough
-self.addEventListener('fetch', e=>{});
